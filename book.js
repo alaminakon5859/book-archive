c5c5f192a381c6c2999bdeb69865747680eb8c42
alaminakon5859 <alaminakon179@gmail.com>
2021-09-02 11:10:28 +0600

    add2

---
+++ book.js
@@ -0,0 +1,54 @@
+const loadBook=()=>{
+    const searchfield =document.getElementById("search-field");
+    const searchfieldtex =searchfield.value ;
+
+    searchfield.value="";
+    
+    const url=`
+    https://openlibrary.org/search.json?q=${searchfieldtex}   
+    `;
+    fetch(url)
+    .then(res => res.json())
+    .then(data => displayloadbook(data.docs));
+
+};
+
+
+
+const displayloadbook=(books)=>{
+
+     const searchresult =document.getElementById("search-result");
+     searchresult.innerText="";
+     books.forEach(book => {
+        //  console.log(book);
+        const div =document.createElement("div");
+        div.classList.add("col");
+        const imgurl =`
+        https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg
+        `
+        div.innerHTML=`
+        <div class="card  "row row-cols-1 row-cols-md-3 g-4"">
+        <img style="height: 350px;" src="${imgurl}" class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">${book.title}</h5>
+          <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+          <button onclick="loadimgdetails('${book.key}')">see details</button>
+        </div>
+      </div>      
+        `;
+        searchresult.appendChild(div);
+         
+     });
+};
+
+
+const loadimgdetails=(details)=>{
+    const urld=`
+    https://openlibrary.org/search.json?q=${details}
+    `;
+     fetch(urld)
+     .then(res => res.json())
+     .then(data => console.log(data));
+    
+
+}
